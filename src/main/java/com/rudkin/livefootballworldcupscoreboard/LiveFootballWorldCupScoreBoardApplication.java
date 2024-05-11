7652e713b6cfb6059805039b5e065efcc1e2ccee
Oleksandr Rudkin <oleksandr.rudkin@gmail.com>
2024-05-11 13:09:23 +0200

    [CE001] | init

---
+++ src/main/java/com/rudkin/livefootballworldcupscoreboard/LiveFootballWorldCupScoreBoardApplication.java
@@ -0,0 +1,13 @@
+package com.rudkin.livefootballworldcupscoreboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiveFootballWorldCupScoreBoardApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiveFootballWorldCupScoreBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/rudkin/livefootballworldcupscoreboard/LiveFootballWorldCupScoreBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rudkin.livefootballworldcupscoreboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LiveFootballWorldCupScoreBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
