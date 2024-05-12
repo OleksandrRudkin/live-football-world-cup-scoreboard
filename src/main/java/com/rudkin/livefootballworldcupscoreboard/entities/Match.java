b0f4a48555c7f7933ad43ea1dc74d74bda9d315c
Oleksandr Rudkin <oleksandr.rudkin@gmail.com>
2024-05-12 17:35:25 +0200

    [CE001] | minor refactor

--- src/main/java/com/rudkin/livefootballworldcupscoreboard/entities/Match.java
+++ src/main/java/com/rudkin/livefootballworldcupscoreboard/entities/Match.java
@@ -13,12 +13,37 @@ public class Match {
   private int homeTeamScore;
   private int awayTeamScore;
 
+  public int calculateTotalScore() {
+    return homeTeamScore + awayTeamScore;
+  }
+
+  public boolean compareByTeamNames(String homeTeam, String awayTeam) {
+    return this.homeTeam.equals(homeTeam) && this.awayTeam.equals(awayTeam);
+  }
+
+  public boolean compareByTeamNames(Match matchToCompare) {
+    return compareByHomeTeamName(matchToCompare) && compareByAwayTeamName(matchToCompare);
+  }
+
+  private boolean compareByAwayTeamName(Match matchToCompare) {
+    return matchToCompare.getAwayTeam().equals(awayTeam);
+  }
+
+  private boolean compareByHomeTeamName(Match matchToCompare) {
+    return matchToCompare.getHomeTeam().equals(homeTeam);
+  }
+
   public void updateScore(int homeTeamScore, int awayTeamScore) {
+    throwIfNegative(homeTeamScore, awayTeamScore);
+
+    this.homeTeamScore = homeTeamScore;
+    this.awayTeamScore = awayTeamScore;
+  }
+
+  private void throwIfNegative(int homeTeamScore, int awayTeamScore) {
     if (homeTeamScore < 0 || awayTeamScore < 0) {
       throw new UpdateScoreException("Score can not be negative");
     }
-    this.homeTeamScore = homeTeamScore;
-    this.awayTeamScore = awayTeamScore;
   }
 
 }
--- src/main/java/com/rudkin/livefootballworldcupscoreboard/matchservice/Scoreboard.java
+++ src/main/java/com/rudkin/livefootballworldcupscoreboard/matchservice/Scoreboard.java
@@ -3,6 +3,7 @@ package com.rudkin.livefootballworldcupscoreboard.matchservice;
 import com.rudkin.livefootballworldcupscoreboard.entities.Match;
 import com.rudkin.livefootballworldcupscoreboard.exception.MatchAlreadyExistsException;
 import com.rudkin.livefootballworldcupscoreboard.exception.NoSuchGameException;
+import com.rudkin.livefootballworldcupscoreboard.exception.UpdateScoreException;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
@@ -21,32 +22,40 @@ public class Scoreboard {
     log.info("Starting a new game between {} and {}", homeTeam, awayTeam);
     var gameToStart = new Match(homeTeam, awayTeam);
 
-    boolean objectExists = matches.stream()
-        .anyMatch(obj -> obj.equals(gameToStart));
+    throwIfExists(gameToStart);
 
-    if (objectExists) {
+    matches.add(gameToStart);
+  }
+
+  private void throwIfExists(Match gameToStart) {
+    if (isGameOnScoreboard(gameToStart)) {
       throw new MatchAlreadyExistsException(
-          String.format("The match %s is already on thr Scoreboard", gameToStart));
-    } else {
-      matches.add(gameToStart);
+          String.format("The match %s is already on the Scoreboard", gameToStart));
     }
   }
 
+  private boolean isGameOnScoreboard(Match gameToStart) {
+    return matches.stream()
+        .anyMatch(game -> game.compareByTeamNames(gameToStart));
+  }
+
   public void updateScore(String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore) {
-    for (Match match : matches) {
-      if (match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam)) {
-        log.info("Updating score for game between {} and {}: {} - {}", homeTeam, awayTeam,
-            homeTeamScore, awayTeamScore);
-        match.updateScore(homeTeamScore, awayTeamScore);
-        break;
-      }
-    }
+
+    Match matchToUpdate = matches.stream()
+        .filter(match -> match.compareByTeamNames(homeTeam, awayTeam))
+        .findFirst()
+        .orElseThrow(() -> new UpdateScoreException(String.format("Match not found: %s vs %s", homeTeam, awayTeam)));
+
+    log.info("Updating score for game between {} and {}: {} - {}", homeTeam, awayTeam,
+                                                                   homeTeamScore, awayTeamScore);
+
+    matchToUpdate.updateScore(homeTeamScore, awayTeamScore);
   }
 
   public void finishGame(String homeTeam, String awayTeam) {
     log.info("Finishing game between {} and {}", homeTeam, awayTeam);
     boolean isNotRemoved = !matches.removeIf(
-        match -> match.getHomeTeam().equals(homeTeam) && match.getAwayTeam().equals(awayTeam));
+        match -> match.compareByTeamNames(homeTeam, awayTeam));
 
     if (isNotRemoved) {
       throw new NoSuchGameException(
@@ -55,14 +64,21 @@ public class Scoreboard {
   }
 
   public List<Match> getSummary() {
-    List<Match> summary = new ArrayList<>(matches);
-    summary.sort(Comparator.comparingInt(
-            (Match match) -> match.getHomeTeamScore() + match.getAwayTeamScore())
-        .reversed()
-        .thenComparing(Comparator.comparingLong((Match match) -> matches.indexOf(match))
-            .reversed()));
     log.info("Retrieving match summary");
-    return summary;
+
+    return matches.stream()
+        .sorted(compareByScoreReversed()
+            .thenComparing(compareByIndexReversed()))
+        .toList();
   }
 
+  private Comparator<Match> compareByIndexReversed() {
+    return Comparator.comparingLong((Match match) -> matches.indexOf(match))
+        .reversed();
+  }
+
+  private Comparator<Match> compareByScoreReversed() {
+    return Comparator.comparingInt(Match::calculateTotalScore)
+        .reversed();
+  }
 }
