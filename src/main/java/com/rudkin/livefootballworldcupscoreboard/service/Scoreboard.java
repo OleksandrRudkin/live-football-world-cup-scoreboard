852f0fbefa1da392144da7daa4dc51defdfc8f0b
Oleksandr Rudkin <oleksandr.rudkin@gmail.com>
2024-05-14 18:06:42 +0200

    [CE001] | added banner, fixed sonar warnings

--- src/main/java/com/rudkin/livefootballworldcupscoreboard/service/Scoreboard.java
+++ src/main/java/com/rudkin/livefootballworldcupscoreboard/service/Scoreboard.java
@@ -5,12 +5,12 @@ import java.util.List;
 
 public interface Scoreboard {
 
-  public void startGame(String homeTeam, String awayTeam);
+   void startGame(String homeTeam, String awayTeam);
 
-  public void updateScore(String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore);
+   void updateScore(String homeTeam, String awayTeam, int homeTeamScore, int awayTeamScore);
 
-  public void finishGame(String homeTeam, String awayTeam);
+   void finishGame(String homeTeam, String awayTeam);
 
-  public List<Match> getSummary();
+   List<Match> getSummary();
 
 }
